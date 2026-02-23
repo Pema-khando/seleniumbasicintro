c863cd739b02e4a3ed9036ae14817b65992c5742
Pema Khando <pemakhando2060@gmail.com>
2026-02-23 12:26:06 +0545

    add basic program

---
+++ src/test/java/javabasicprogram/BasicProgram.java
@@ -0,0 +1,10 @@
+package javabasicprogram;
+
+public class BasicProgram {
+    public static void main(String[] args) {
+        int a=5;
+        int b=2;
+        int sum = a+b;
+        System.out.println("the sum is:"+sum);
+    }
+}
